28a71b14f94112fb1b63cc72ed28a9afacb964d3
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-03-25 19:59:37 -0300

    Created Department Class

---
+++ src/application/App.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Department dp = new Department(1, "Jogos");
+	
+		System.out.println(dp);
+	}
+
+}
