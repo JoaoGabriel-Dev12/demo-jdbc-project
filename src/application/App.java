1d706955c9f2914ffce3477b33df3c89c904ba14
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-03-25 20:08:01 -0300

    Created Seller Class

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
 
@@ -9,7 +12,8 @@ public class App {
 		
 		Department dp = new Department(1, "Jogos");
 	
-		System.out.println(dp);
+		Seller seller = new Seller(21, "Joao", "joao@emial", new Date(), 8900.65, dp);
+		System.out.println(seller);
 	}
 
 }
