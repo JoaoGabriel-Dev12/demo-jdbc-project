43298b2cefc7cef8cbd86a9ef5df71ab4b073513
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-03-26 22:34:36 -0300

    method findById and update for department

--- src/application/App2.java
+++ src/application/App2.java
@@ -10,12 +10,23 @@ public class App2 {
 		
 		DepartmentDao dao = DaoFactory.createDepartmentDao();
 		
+		/*
 		System.out.println("========== TESTE DO DEPARTMENT ==========");
 		System.out.println("-------------- Teste 1 -> insert department --------------");
 		Department dep = new Department(null, "Music");
 		dao.insert(dep);
 		System.out.println("insert runned! id = " +dep.getId());
-
+		*/
+		
+		System.out.println("-------------- Teste 2 -> get department by id--------------");
+		Department dep = dao.findById(4);
+		System.out.println(dep);
+		
+		System.out.println("\n-------------- Teste 3 -> update department--------------");
+		Department depUp = dao.findById(6);
+		depUp.setName("Backend");
+		dao.update(depUp);
+		System.out.println("Updated complete!");
 	}
 
 }
